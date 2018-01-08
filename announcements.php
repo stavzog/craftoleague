0d6fbe201d267e098df81337cfccf2bf34967e2f
CraftOLeague <stavzog@gmail.com>
2018-01-08 22:17:02 +0200

    Add files via upload

---
+++ announcements.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ANNOUNCEMENTS | Craft O' League</title>
+	<link rel="stylesheet" href="master.css">
+	<link href="https://fonts.googleapis.com/css?family=Berkshire+Swash|Leckerli+One|Merienda+One|Norican|Pacifico" rel="stylesheet"
+</head>
+<body>
+	<div class="hero-img">
+			<div class="hero-txt">
+				<img src="McAvatar.png">
+				<h1>Craft O' League</h1>
+			</div>
+		</div>
+		<nav>
+			<ul>
+				<li class="active"><a href="index.html">HOME</a></li>
+				<li><a href="join.html">JOIN</a></li>
+				<li><a href="members.html">MEMBERS</a></li>
+				<li><a href="announcements.php">ANNOUNCEMENTS</a></li>
+
+			</ul>
+		</nav>
+		<br><br><br>
+		<div class="main">
+			<?ph
+				include announcementsFile.html
+			<?p
+		</div>
+</body>
+</html>
